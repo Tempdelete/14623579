88982fce2f77e9954a01a3c6bdd760fdecc31a39
Nico Katzke <Nicktz@users.noreply.github.com>
2021-04-19 18:25:10 +0200

    Update Data_Create.R
    Added df and return(df)

--- code/Data_Create.R
+++ code/Data_Create.R
@@ -1,4 +1,5 @@
 Data_Create <- function(N = 100) {
+  df <-
   data.frame(
   Height_Score = rnorm(N,5,1),
   Weight_Score = rnorm(N,5,1),
@@ -8,5 +9,7 @@ Data_Create <- function(N = 100) {
                                 ifelse( "Height_Score" >= 4, 4,
                                         ifelse("Height_Score" >= 3, 5 , 6))))
   ) %>% tbl_df()
+
+  return(df)
 
 }
